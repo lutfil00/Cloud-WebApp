b83c35dbbb791128ab783869e4b6dbeefc378fcb
lutfil00 <ltfhadi00@gmail.com>
2024-06-02 10:54:15 +0800

    Adjusting database config

--- db.php
+++ db.php
@@ -21,8 +21,9 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 // Check connection
 if ($conn === false) {
     // Handle connection errors
-    die(print_r(sqlsrv_errors(), true));
+    die("Connection could not be established.<br />" . print_r(sqlsrv_errors(), true));
 } 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['name'];
     $number = $_POST['number'];
@@ -32,12 +33,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $address = $_POST['address'];
     $message = $_POST['message'];
 
-    $sql = "INSERT INTO order_query (name, number, food, extra, orders, address, message) VALUES (?, ?)";
+    $sql = "INSERT INTO dbo.order_query (name, number, food, extra, orders, address, message) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $params = array($username, $number, $food, $extra, $orders, $address, $message);
     
     $stmt = sqlsrv_query($conn, $sql, $params);
 
     if ($stmt === false) {
+        echo "Error in statement execution.<br />";
         die(print_r(sqlsrv_errors(), true));
     } else {
         echo "Data inserted successfully!";
@@ -45,6 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     sqlsrv_free_stmt($stmt);
 }
+
 // Close the connection
 sqlsrv_close($conn);
 ?>
